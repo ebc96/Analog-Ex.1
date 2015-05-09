6a0b8bffc8c5caabbb2e56e10dd77edf43dbb108
ebc96 <bonete.info4@gmail.com>
2015-05-09 19:53:17 +0200

    Ex1

---
+++ Analog_Ex.1/Analog_Ex.1.ino
@@ -0,0 +1,117 @@
+//****** Variables *******************************************************
+
+
+
+//****** Setup ***********************************************************
+void setup (){  
+pinMode (13, OUTPUT);
+pinMode (12, OUTPUT);
+pinMode (11, OUTPUT);
+pinMode (6, OUTPUT);
+pinMode (5, OUTPUT);
+pinMode (4, OUTPUT);
+}
+
+//****** Loop ***********************************************************
+void loop (){
+ 
+analogWrite(13, 5);
+analogWrite(12, 5);
+analogWrite(11, 5);
+analogWrite(6, 5);
+analogWrite(5, 5);
+analogWrite(4, 5);
+delay(200);
+analogWrite(13, 40);
+analogWrite(12, 40);
+analogWrite(11, 40);
+analogWrite(6, 40);
+analogWrite(5, 40);
+analogWrite(4, 40);
+delay(200);
+analogWrite(13, 80);
+analogWrite(12, 80);
+analogWrite(11, 80);
+analogWrite(6, 80);
+analogWrite(5, 80);
+analogWrite(4, 80);
+delay(200);
+analogWrite(13, 110);
+analogWrite(12, 110);
+analogWrite(11, 110);
+analogWrite(6, 110);
+analogWrite(5, 110);
+analogWrite(4, 110);
+delay(200);
+analogWrite(13, 140);
+analogWrite(12, 140);
+analogWrite(11, 140);
+analogWrite(6, 140);
+analogWrite(5, 140);
+analogWrite(4, 140);
+delay(200);
+analogWrite(13, 190);
+analogWrite(12, 190);
+analogWrite(11, 190);
+analogWrite(6, 190);
+analogWrite(5, 190);
+analogWrite(4, 190);
+delay(200);
+analogWrite(13, 200);
+analogWrite(12, 200);
+analogWrite(11, 200);
+analogWrite(6, 200);
+analogWrite(5, 200);
+analogWrite(4, 200);
+delay(200);
+analogWrite(13, 255);
+analogWrite(12, 255);
+analogWrite(11, 255);
+analogWrite(6, 255);
+analogWrite(5, 255);
+analogWrite(4, 255);
+delay(200);
+analogWrite(13, 200);
+analogWrite(12, 200);
+analogWrite(11, 200);
+analogWrite(6, 200);
+analogWrite(5, 200);
+analogWrite(4, 200);
+delay(200);
+analogWrite(13, 190);
+analogWrite(12, 190);
+analogWrite(11, 190);
+analogWrite(6, 190);
+analogWrite(5, 190);
+analogWrite(4, 190);
+delay(200);
+analogWrite(13, 140);
+analogWrite(12, 140);
+analogWrite(11, 140);
+analogWrite(6, 140);
+analogWrite(5, 140);
+analogWrite(4, 140);
+delay(200);
+analogWrite(13, 110);
+analogWrite(12, 110);
+analogWrite(11, 110);
+analogWrite(6, 110);
+analogWrite(5, 110);
+analogWrite(4, 110);
+delay(200);
+analogWrite(13, 80);
+analogWrite(12, 80);
+analogWrite(11, 80);
+analogWrite(6, 80);
+analogWrite(5, 80);
+analogWrite(4, 80);
+delay(200);
+analogWrite(13, 40);
+analogWrite(12, 40);
+analogWrite(11, 40);
+analogWrite(6, 40);
+analogWrite(5, 40);
+analogWrite(4, 40);
+delay(200);
+}
+
